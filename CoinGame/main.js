b22586fce30dfe14bc3cf4ec803bb2309c959e2a
Shaxrux <shaxrux243@gmail.com>
2025-02-01 16:32:18 +0500

    Add files via upload

---
+++ CoinGame/main.js
@@ -0,0 +1,25 @@
+let coinClick = document.querySelector("#coin-click");
+let balanceView = document.querySelector(".balance");
+let sendBalance = document.querySelector("#send-balance")
+
+let userBalance = 0
+let oneClick = 1
+let clickLimit = 100
+
+let tg = window.Telegram.WebApp;
+tg.expand()
+
+coinClick.addEventListener("click", () => {
+  userBalance += oneClick;
+  balanceView.innerHTML = `
+  <h2>Balance: ${userBalance}</h2>
+  `
+})
+
+sendBalance.addEventListener("click", () => {
+  let data = {
+    balance: userBalance
+  }
+  tg.sendData(JSON.stringify(data));
+  tg.close()
+})
